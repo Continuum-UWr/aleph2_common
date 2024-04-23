391a05a2398919cba8d885ca65079a4ce360b87b
Paweł Dybiec <pdybiec@stud.cs.uni.wroc.pl>
2024-04-23 11:50:46 +0000

    Add nix flake

---
+++ input_manager/default.nix
@@ -0,0 +1,30 @@
+{ buildRosPackage, ament-cmake, rosidl-default-generators
+, rosidl-default-runtime, ament-index-cpp, rclcpp, SDL2, yaml-cpp, libudev-zero
+, ament-lint-auto, ament-cmake-cpplint, ament-cmake-lint-cmake
+, ament-cmake-uncrustify }:
+buildRosPackage {
+  pname = "input_manager";
+  version = "0.0.0";
+
+  src = ./.;
+
+  doCheck = true;
+
+  buildType = "ament_cmake";
+  buildInputs = [ ament-cmake rosidl-default-generators ];
+  propagatedBuildInputs = [
+    rosidl-default-runtime
+    ament-index-cpp
+    rclcpp
+    SDL2
+    yaml-cpp
+    libudev-zero
+  ];
+  checkInputs = [
+    ament-lint-auto
+    ament-cmake-cpplint
+    ament-cmake-lint-cmake
+    ament-cmake-uncrustify
+  ];
+  nativeBuildInputs = [ ament-cmake ];
+}
