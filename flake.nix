f0a2f36d32778b061fcc11c1d36071c6fe17e18c
Błażej Sowa <bsowa123@gmail.com>
2025-12-21 17:07:45 +0100

    Reformat

--- flake.nix
+++ flake.nix
@@ -4,13 +4,21 @@
     flake-utils.follows = "nix-ros-overlay/flake-utils";
     nix-ros-overlay.url = "github:lopsided98/nix-ros-overlay";
   };
-  outputs = { self, nixpkgs, flake-utils, nix-ros-overlay }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      nix-ros-overlay,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
-        pkgs = (import nixpkgs {
-          system = system;
-          overlays = [ nix-ros-overlay.overlays.default ];
-        }).pkgs;
+        pkgs =
+          (import nixpkgs {
+            system = system;
+            overlays = [ nix-ros-overlay.overlays.default ];
+          }).pkgs;
         ros = pkgs.rosPackages.jazzy;
 
         aleph2-description = ros.callPackage (import ./aleph2_description) { };
@@ -18,17 +26,28 @@
         input-manager = ros.callPackage (import ./input_manager) { };
 
         devEnv = ros.buildEnv {
-          paths =
-            [ ros.ros-core aleph2-description aleph2-teleop input-manager ];
+          paths = [
+            ros.ros-core
+            aleph2-description
+            aleph2-teleop
+            input-manager
+          ];
         };
 
-      in {
+      in
+      {
         packages = {
-          inherit aleph2-description aleph2-teleop input-manager devEnv;
+          inherit
+            aleph2-description
+            aleph2-teleop
+            input-manager
+            devEnv
+            ;
           default = input-manager;
         };
 
         devShells.default = pkgs.mkShell { nativeBuildInputs = [ devEnv ]; };
         formatter = pkgs.nixfmt;
-      });
+      }
+    );
 }
